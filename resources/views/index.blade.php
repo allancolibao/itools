55765c1db9df36cee68fcef7bf7d5e8ae0578403
allancolibao <colibaoallanreyes@gmail.com>
2020-02-27 07:45:51 +0800

    commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>iTools</title>
+
+        <!-- Page level custom style -->
+        <link href="{{asset('css/style.css')}}" rel="stylesheet">
+
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                   <h1>iTools</h1>
+                </div>
+
+            </div>
+        </div>
+
+        <!-- Page level custom scripts -->
+        <script type="text/javascript" src="{{ asset('js/custom.js') }}"></script>
+    </body>
+</html>
